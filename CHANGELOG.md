8b55ad7a607b421d191cba4a728f6d6b314ba5b2
Brian DeHamer <brian@dehamer.com>
2015-01-29 10:08:31 -0800

    Miscellany

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+0.1.0 - 2015-01-29
+-------------------
+Initial beta release
--- main.go
+++ main.go
@@ -51,26 +51,3 @@ func dockerEndpoint() string {
 
 	return endpoint
 }
-
-// For streaming responses
-//type flushWriter struct {
-//flusher http.Flusher
-//writer  io.Writer
-//}
-
-//func newFlushWriter(writer io.Writer) flushWriter {
-//fw := flushWriter{writer: writer}
-//if flusher, ok := writer.(http.Flusher); ok {
-//fw.flusher = flusher
-//}
-
-//return fw
-//}
-
-//func (fw *flushWriter) Write(p []byte) (n int, err error) {
-//n, err = fw.writer.Write(p)
-//if fw.flusher != nil {
-//fw.flusher.Flush()
-//}
-//return
-//}
