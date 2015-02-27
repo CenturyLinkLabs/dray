9283d1ea7a1969a477c5cff31000790abf8936f1
Laura Frank <ljfrank@gmail.com>
2015-02-27 10:38:29 -0600

    Hero scrolling and smooth navbar transition

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,39 +1,48 @@
 $(document).ready(function() {
 
-  //Navigation
+  function sideNav() {
+    $('#nav-toggle').sidr({
+      name: 'main-nav',
+      source: '#navigation',
+      side: 'right'
+    });
 
-  $('#nav-toggle').sidr({
-    name: 'main-nav',
-    source: '#navigation',
-    side: 'right'
-  });
-
-  $('#sidr-id-nav-close').click(function () {
-    $.sidr('close', 'main-nav');
-  });
-
-  $(window).touchwipe({
-    wipeRight: function() {
+    $('#sidr-id-nav-close').click(function () {
       $.sidr('close', 'main-nav');
-    },
-    wipeLeft: function() {
-      $.sidr('open', 'main-nav');
-    },
-    preventDefaultEvents: false
-  });
+    });
 
-
-  var $header = $('header');
-      height = $('header').height();
-      size = window.matchMedia('all and (min-width: 650px)');
-  
-  if(size.matches) {
-    $(window).scroll(function() {
-      if( $(this).scrollTop() > height ) {
-        $header.addClass('small');
-      } else {
-        $header.removeClass('small');
-      }
+    $(window).touchwipe({
+      wipeRight: function () {
+        $.sidr('close', 'main-nav');
+      },
+      wipeLeft: function () {
+        $.sidr('open', 'main-nav');
+      },
+      preventDefaultEvents: false
     });
   }
+
+  function heroScroll(){
+    var $header = $('header');
+    var $hero = $('.hero');
+    var height = $hero.css("height");
+
+    $('section:nth-of-type(2)').css('margin-top', height);
+
+    var size = window.matchMedia('all and (min-width: 650px)');
+
+    if(size.matches) {
+      $(window).scroll(function() {
+        if( $(this).scrollTop() > height.replace('px', '') ) {
+          $header.addClass('small');
+        } else {
+          $header.removeClass('small');
+        }
+      });
+    }
+  }
+
+  sideNav();
+  heroScroll();
+
 });
