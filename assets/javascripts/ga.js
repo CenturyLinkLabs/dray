c39e45ce8c84e8fe3d9a0d0f5aab7548394ef6ac
Parker Fleming <parker.fleming@gmail.com>
2015-03-06 15:07:36 -0700

    add link underlines & add Google Analytic events for outbound links

--- assets/javascripts/ga.js
+++ assets/javascripts/ga.js
@@ -7,3 +7,17 @@ _gaq.push(['_trackPageview']);
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+
+
+/**
+* Function that tracks a click on an outbound link in Google Analytics.
+* This function takes a valid URL string as an argument, and uses that URL string
+* as the event label.
+*/
+var trackOutboundLink = function(url) {
+   ga('send', 'event', 'outbound', 'click', url, {'hitCallback':
+     function () {
+     document.location = url;
+     }
+   });
+}
